d525697559c00375757643703b0067520788aa75
s7123 <s7123@foxmail.com>
2025-03-03 23:26:32 +0800

    修复小bug

--- app/controllers/admin_login.php
+++ app/controllers/admin_login.php
@@ -9,7 +9,7 @@ $password = $_POST['password'];
 if (Auth::attempt($username, $password)) {
     // 登录成功，重定向到设置页面
     $_SESSION['user'] = $username;
-    header('Location: /admin/');
+    header('Location: /admin');
     exit;
 } else {
     // 登录失败，重定向回登录页面
